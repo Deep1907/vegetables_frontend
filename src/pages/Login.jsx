c8093d8f46a039c6ef6264e8d521b37959ce4017
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-13 23:08:12 +0530

    again changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,6 +17,8 @@ const Login = () =>{
         console.log(response)
 
         if(response.data.success){
+            localstorage.setItem("token",response.data.token)
+            localStorage.setItem("username", response.data.username);
             navigate("/")
         }
 
