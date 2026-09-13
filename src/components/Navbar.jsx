534bd95661c3ce390ce5123244e5848b30e3dede
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-13 20:48:44 +0530

    again changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,6 +30,7 @@ const Navbar = () => {
                             <li><Link to="/services">Services</Link></li>  
                             <li><Link to="/contact">Contact</Link></li>  
                             <li><Link to="/signup">Signup</Link></li>  
+                            <li><Link to="/login">Login</Link></li>  
                              
                         </ul>
                     </div>
