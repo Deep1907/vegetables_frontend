bd4891c4b2d60c720cefe4b95806af1e2cd2e66d
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-14 23:26:22 +0530

    again changed

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Login from "./pages/Login.jsx"
 import protectedRoute from './route/protectedRoute.jsx'
 
 export const backendURL = "https://vegetables-backend.onrender.com";
+import { Navigate } from 'react-router-dom'
 
 function App() {
 
